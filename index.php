df2ac9616d43af8149970aa7b333c911ae08ffa4
FRA2721 <cimino.francesco36@gmail.com>
2022-11-30 09:42:38 +0100

    user hotels optional completed

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- hotels array list -->
+<!-- hotel array data -->
 <?php
 $hotels = [
 
@@ -47,19 +47,31 @@ $hotels = [
         'distance_to_center' => 50
     ],
 ];
+// /hotel array data
+
+// methods to get the user choice about hotel optional
+// parking optional (GET method): if the user wants a hotel with parking zone
+$parking = $_GET["parking"] ?? "";
+
+// vote (GET method): if the user wants to filter the hotels by their votes
+$vote = $_GET["vote"] ?? "";
 ?>
-<!-- /hotels array list -->
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- bootstrap cdn -->
+
+    <!-- import bootstrap  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- /bootstrap cdn -->
+    <!-- /import bootstrap -->
+
+    <!-- css -->
+    <link rel="stylesheet" href="style.css">
+    <!-- /css -->
 
     <!-- title -->
     <title>PHP Hotels</title>
@@ -68,91 +80,179 @@ $hotels = [
 </head>
 
 <body>
+    
+    <h1>HOTELS</h1>
 
-    <!-- form section to select the hotel that the user prefer -->
-    <form action="index.php" method="GET">
-        <label for="parking">Parking</label>
+    <!-- hotels table data -->
+    <table class="table table-striped table-dark">
 
-        <!-- user parking choice -->
-        <select name="parking" id="parking">
-            <option value="0">...</option>
-            <option value="1">Yes</option>
-            <option value="2">No</option>
-        </select>
-        <!-- /user parking choice -->
-
-        <!-- user vote choice -->
-        <label for="vote">Vote</label>
-        <select name="vote" id="vote">
-            <option value="">...</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-        </select>
-        <!-- /user voice choice -->
-
-        <button type="submit">Search</button>
-    </form>
-    <!-- /form section to select the hotel that the user prefer -->
-
-    <!-- hotels table -->
-    <table class="table">
-
-        <!-- table head -->
+        <!-- hotels table head data -->
         <thead>
             <tr>
                 <!-- hotels voices data iteration -->
-                <?php foreach ($hotels[0] as $key => $value /*voices-data*/) { ?>
+                <?php foreach ($hotels[0] as $key => $value) { ?>
                     <th scope="col"><?php echo $key; ?></th>
                 <?php } ?>
                 <!-- /hotels voices data iteration -->
             </tr>
         </thead>
-        <!-- /table head -->
+        <!-- /hotels table head data -->
 
-        <!-- table body -->
+        <!-- hotels table body data -->
         <tbody>
-
-            <!-- hotels itereation data -->
-            <?php foreach ($hotels as $hotel) { ?>
-                <tr>
-                    <?php foreach ($hotel as $key => $hotelData) { ?>
-                        <!-- hotel name section -->
-                        <!-- name data -->
-                        <?php if ($key === "name") { ?>
-                            <th scope="row"><?php echo $hotelData; ?></th>
-                        <?php } else { ?>
-
-                            <!-- hotel parking section -->
-                            <!-- parking true || false data -->
-                            <?php if ($key === "parking") { ?>
-
-                                <!-- true -> yes -->
-                                <?php if ($hotelData === true) { ?>
-                                    <td>Yes</td>
-
-                                <!-- false -> no -->
-                                <?php } else { ?>
-                                    <td>No</td>
-                                <?php } ?>
-
-                            <!-- others hotels voices data -->
-                            <?php } else { ?>
-                                <td><?php echo $hotelData; ?></td>
-                            <?php } ?>
-                        <?php } ?>
-                    <?php } ?>
-                </tr>
-            <?php } ?>
-            <!-- /hotels iteration data -->
-            
+            <?php
+            // used foreach inside another foreach to reiterate the operations for every item ($hotel) inside the array ($hotels)
+            foreach ($hotels as $hotel) {
+                // method to GET user choice about optional
+                // check
+                if ($parking === "" && $vote === "") {
+                    echo "<tr>";
+                    foreach ($hotel as $key => $hotel) {
+                        // hotels name voice data
+                        if ($key === "name") {
+                            echo "<th scope='row'>$hotel</th>";
+                        } else {
+                            // if the voice is 'parking' -> ...
+                            // true || false parking data
+                            if ($key === "parking") {
+                                // true
+                                if ($hotel == true) {
+                                    echo "<td>Yes</td>";
+                                } else {
+                                    // false
+                                    echo "<td>No</td>";
+                                }
+                            } else {
+                                // other hotel data
+                                echo "<td>$hotel</td>";
+                            }
+                        }
+                    }
+                    echo "</tr>";
+                } else {
+                    // method to GET user choice about optional (none)
+                    if ($parking != "" && $vote != "") {
+                        // check
+                        if ($hotel['parking'] == $parking && $hotel['vote'] == $vote) {
+                            echo "<tr>";
+                            foreach ($hotel as $key => $hotel) {
+                                // hotels name voice data
+                                if ($key === "name") {
+                                    echo "<th scope='row'>$hotel</th>";
+                                } else {
+                                    // if the voice is 'parking' -> ...
+                                    // true || false parking data
+                                    if ($key === "parking") {
+                                        // true
+                                        if ($hotel == true) {
+                                            echo "<td>Yes</td>";
+                                        } else {
+                                            // false
+                                            echo "<td>No</td>";
+                                        }
+                                    } else {
+                                        // other hotels data
+                                        echo "<td>$hotel</td>";
+                                    }
+                                }
+                            }
+                            echo "</tr>";
+                        }
+                    } else {
+                        // parking optional method
+                        if ($parking === "") {
+                            // vote optional method
+                            if ($vote == $hotel['vote']) {
+                                echo "<tr>";
+                                foreach ($hotel as $key => $hotel) {
+                                    // hotels name voice data
+                                    if ($key === "name") {
+                                        echo "<th scope='row'>$hotel</th>";
+                                    } else {
+                                        // if the voice is 'parking' -> ...
+                                        // true || false parking data
+                                        if ($key === "parking") {
+                                            // true
+                                            if ($hotel == true) {
+                                                echo "<td>Yes</td>";
+                                            } else {
+                                                // false
+                                                echo "<td>No</td>";
+                                            }
+                                        } else {
+                                            // other hotels data
+                                            echo "<td>$hotel</td>";
+                                        }
+                                    }
+                                }
+                                echo "</tr>";
+                            }
+                        } else {
+                            // parking check
+                            if ($parking == $hotel['parking']) {
+                                echo "<tr>";
+                                foreach ($hotel as $key => $hotel) {
+                                    // hotel name voice data
+                                    if ($key === "name") {
+                                        echo "<th scope='row'>$hotel</th>";
+                                    } else {
+                                        // if the voice is 'parking' -> ...
+                                        // true || false parking data
+                                        if ($key === "parking") {
+                                            // true
+                                            if ($hotel == true) {
+                                                echo "<td>Yes</td>";
+                                            } else {
+                                                // false
+                                                echo "<td>No</td>";
+                                            }
+                                        } else {
+                                            // other hotels data
+                                            echo "<td>$hotel</td>";
+                                        }
+                                    }
+                                }
+                                echo "</tr>";
+                            }
+                        }
+                    }
+                }
+            }
+            ?>
         </tbody>
-        <!-- /table body -->
+        <!-- /hotels table body data -->
 
     </table>
-    <!-- /hotels table -->
+    <!-- /hotel table data -->
+
+    <!-- form user choice about hotels optional data  -->
+    <form action="index.php" method="GET">
+
+    <!-- hotels parking data -->
+    <label for="parking-optional">Parking zone optional</label>
+    <select name="parking" id="parking-optional">
+        <option value="">Yes/No</option>
+        <option value="1">Yes</option>
+        <option value="2">No</option>
+    </select>
+    <!-- /hotels parking data -->
+
+    <!-- hotels vote data -->
+    <label for="vote-optional">Vote</label>
+    <select name="vote" id="vote-optional">
+        <option value=""></option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select>
+    <!-- /hotels vote data -->
+
+    <!-- search button -->
+    <button type="submit">Search my Hotel</button>
+    </form>
+    <!-- /form user choice about hotels optional data  -->
 
 </body>
 
